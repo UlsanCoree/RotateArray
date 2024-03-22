b8a40c80867473778fc5a2262238bb1083d158d8
Kim SeungHyun <korafa@naver.com>
2024-03-23 08:09:22 +0900

    초기 커밋.

---
+++ RotateArray/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace RotateArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputArray = { 1, 2, 3, 4, 5 };
+            int[] rotateArray = RotateArray(inputArray);
+
+            Console.WriteLine(string.Join(", ", rotateArray));
+        }
+
+        public static int[] RotateArray(int[] array)
+        {
+            int[] rotateArray = new int[array.Length];
+
+            rotateArray[array.Length - 1] = array[0];
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                rotateArray[i] = array[i + 1];     
+            }
+
+            return rotateArray;
+        }
+    }
+}
